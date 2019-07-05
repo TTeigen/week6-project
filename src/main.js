652cf9eac71ee989e8191b21f0350640c2beadb1
Trevor Teigen <trevor.teigen123@gmail.com>
2019-07-05 14:22:54 -0700

    finished logic

--- src/main.js
+++ src/main.js
@@ -4,10 +4,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { DoctorSearch } from './backend-code';
-//variables
-let body;
-let bio;
-//code
 
 $(function() {
 
@@ -26,16 +22,26 @@ $(function() {
     }
     let results = search.searchBySymptom(symptom,gender,specialty);
     results.then (function(response){
-      body = JSON.parse(response);
-
+      let body = JSON.parse(response);
+      console.log("body");
+      console.log(body);
       if (body.data.length > 0) {
         $('#docs').text(body.data.length);
         for (let i = 0; i < body.data.length; i++) {
+          let patients;
           let pic = body.data[i].profile.image_url;
           let name = `${body.data[i].profile.first_name} ${body.data[i].profile.last_name}, ${body.data[i].profile.title}`;
-          bio = `${body.data[i].profile.bio}`;
+          let phone = body.data[i].practices[0].phones[0].number;
+          let address =` ${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.street2}, ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state}, ${body.data[i].practices[0].visit_address.zip}`;
+          if(body.data[i].practices[0].accepts_new_patients === true) {
+            patients = "Yes";
+          } else {
+            patients = "No";
+          }
           $('.results').append(`<div class= 'doc${i}'>${name}<br>`);
-          $('.results').append(`<img src="${pic}"></div><hr>`);
+          $('.results').append(`<img src="${pic}">`);
+          $('.results').append(`<div class = 'bio'>Phone: ${phone},<br>Address: ${address},<br>Accepting Patients: ${patients} </div></div><hr>`);
+
         }
       } else {
         $('.results').append(`<p> Sorry! Unfortunately your search has not matched with any doctors. Perhaps try a different search term, or search by specialty (eg, pediatrician)</p>`)
